5c1bd544b23722f9c69e154efee1a168eaeb9475
depdrawers <paz6@hotmail.com>
2023-01-02 23:22:20 +0000

    Add project files.

---
+++ Paranormal Footage Credibility System/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace Paranormal_Footage_Credibility_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.longRating = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_clr = new System.Windows.Forms.Button();
+            this.btn_showRating = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.quickRating = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.scoreValue = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_about = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_tips = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // checkListBox1
+            // 
+            this.checkListBox1.CheckOnClick = true;
+            this.checkListBox1.FormattingEnabled = true;
+            this.checkListBox1.Items.AddRange(new object[] {
+            "No clear reason OR flimsy reason(s) for camera operator to be recording?",
+            "Footage is shot in portrait mode?",
+            "Trail Camera Footage?",
+            "CCTV Footage?",
+            "Footage takes place at night, or with limited visibility?",
+            "Footage uses Infra Red or similar night camera technology?",
+            "Footage ends abruptly without explanation?",
+            "Footage is cropped, has deliberate edits or lacks context?",
+            "Footage comes with elaborate backstory?",
+            "\"There was no one else present at the time\"?",
+            "Footage is from CCTV that has no clear reason to exist- e.g. CCTV in a residentia" +
+                "l home looking at nothing in particular?",
+            "Camera operator makes no attempt to approach subject where reasonably applicable?" +
+                "",
+            "Footage is unreasonably shakey and / or contains unnecessary zooms OR camera oper" +
+                "ator looks away from the subject?",
+            "Footage is from Middle East, Mexico or Brazil?",
+            "Footage resolution is lower than 720p?",
+            "Footage depicts known graphical errors / technical glitches?",
+            "Frames Per Second of Footage is less than 10fps?",
+            "Footage has large amount of digital compression?",
+            "Subject of footage is indistinct and / or some distance away?",
+            "Footage is CCTV footage recorded off a screen with a phone?",
+            "Footage is from a Redditor?",
+            "Footage is from a TikToker?",
+            "Footage is from a Youtuber?",
+            "Footage is from \"Paranormal Investigator\" Youtuber or similar?",
+            "Footage only contains \"mysterious sounds\", no visual phenomenon?"});
+            this.checkListBox1.Location = new System.Drawing.Point(46, 32);
+            this.checkListBox1.Name = "checkListBox1";
+            this.checkListBox1.Size = new System.Drawing.Size(682, 148);
+            this.checkListBox1.TabIndex = 0;
+            this.checkListBox1.SelectedIndexChanged += new System.EventHandler(this.checkListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(529, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select all check boxes applicable to footage being tested. Leave unchecked if uns" +
+    "ure of that option:";
+            // 
+            // longRating
+            // 
+            this.longRating.Location = new System.Drawing.Point(46, 260);
+            this.longRating.Multiline = true;
+            this.longRating.Name = "longRating";
+            this.longRating.ReadOnly = true;
+            this.longRating.Size = new System.Drawing.Size(681, 70);
+            this.longRating.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Credibility Rating Output:";
+            // 
+            // btn_clr
+            // 
+            this.btn_clr.Location = new System.Drawing.Point(182, 195);
+            this.btn_clr.Name = "btn_clr";
+            this.btn_clr.Size = new System.Drawing.Size(130, 34);
+            this.btn_clr.TabIndex = 4;
+            this.btn_clr.Text = "Clear All";
+            this.btn_clr.UseVisualStyleBackColor = true;
+            this.btn_clr.Click += new System.EventHandler(this.btn_clr_Click);
+            // 
+            // btn_showRating
+            // 
+            this.btn_showRating.Location = new System.Drawing.Point(46, 195);
+            this.btn_showRating.Name = "btn_showRating";
+            this.btn_showRating.Size = new System.Drawing.Size(130, 34);
+            this.btn_showRating.TabIndex = 5;
+            this.btn_showRating.Text = "Calculate Rating!";
+            this.btn_showRating.UseVisualStyleBackColor = true;
+            this.btn_showRating.Click += new System.EventHandler(this.btn_showRating_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(318, 195);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(130, 34);
+            this.btn_reset.TabIndex = 6;
+            this.btn_reset.Text = "Reset System";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Overall Rating:";
+            // 
+            // quickRating
+            // 
+            this.quickRating.AutoSize = true;
+            this.quickRating.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.quickRating.Location = new System.Drawing.Point(46, 360);
+            this.quickRating.Name = "quickRating";
+            this.quickRating.Size = new System.Drawing.Size(0, 32);
+            this.quickRating.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(636, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Score:";
+            // 
+            // scoreValue
+            // 
+            this.scoreValue.AutoSize = true;
+            this.scoreValue.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.scoreValue.Location = new System.Drawing.Point(568, 360);
+            this.scoreValue.Name = "scoreValue";
+            this.scoreValue.Size = new System.Drawing.Size(74, 45);
+            this.scoreValue.TabIndex = 10;
+            this.scoreValue.Text = "100";
+            this.scoreValue.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(646, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 45);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "/ 100";
+            // 
+            // btn_about
+            // 
+            this.btn_about.Location = new System.Drawing.Point(454, 195);
+            this.btn_about.Name = "btn_about";
+            this.btn_about.Size = new System.Drawing.Size(130, 34);
+            this.btn_about.TabIndex = 12;
+            this.btn_about.Text = "About";
+            this.btn_about.UseVisualStyleBackColor = true;
+            this.btn_about.Click += new System.EventHandler(this.btn_about_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 430);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(585, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "PFC Rating System and application created by DDD Wares, November 2021 - Contact o" +
+    "n twitter: @depdrawers";
+            // 
+            // btn_tips
+            // 
+            this.btn_tips.Location = new System.Drawing.Point(590, 195);
+            this.btn_tips.Name = "btn_tips";
+            this.btn_tips.Size = new System.Drawing.Size(137, 34);
+            this.btn_tips.TabIndex = 14;
+            this.btn_tips.Text = "Tips!";
+            this.btn_tips.UseVisualStyleBackColor = true;
+            this.btn_tips.Click += new System.EventHandler(this.btn_tips_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 454);
+            this.Controls.Add(this.btn_tips);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btn_about);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.scoreValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.quickRating);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_showRating);
+            this.Controls.Add(this.btn_clr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.longRating);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkListBox1);
+            this.Name = "Form1";
+            this.Text = "Paranormal Footage Credibility Rating System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox longRating;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_clr;
+        private System.Windows.Forms.Button btn_showRating;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label quickRating;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label scoreValue;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_about;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_tips;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Paranormal Footage Credibility System/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paranormal_Footage_Credibility_System
+{
+    public partial class Form1 : Form
+    {
+        public int currentRating = 100;//all footage starts at 100 points credibility and is reduced through affirmative answers to questions
+
+        public string[] ratingOutputs = new string[] {
+            "Strenuous rigour, intense scrutiny and sources cited required to refute footage / claims.",
+            "Elevated levels of scrutiny, reasonable detail required for counter-explanation or debunking.",
+            "Reasonable, non-paranormal explanations are to be accepted over the paranormal.",
+            "Only brief, non-paranormal explanations are required to refute footage / claims.",
+            "Footage lacks credibility and is to be dismissed out of hand.",
+            "Footage to be subjected to intense mockery."
+        };
+        public string[] quickRatings = new string[] {
+            "Possibly credible footage...",
+            "Possibly authentic or high-quality fake",
+            "Unlikely to be 100% authentic",
+            "Likely fake",
+            "Absolutely fake",
+            "Absolute BULLSHIT!!!"
+        };
+
+        public Dictionary<string, int> checkListDictionary = new Dictionary<string, int>();
+
+
+        private void FufillCheckListValues() {//create questions to check credibilty
+                                              //affirmative answers to the undernoted erode credibility according to the weighting values
+                                              //dictionary TValue represents weighting
+            checkListDictionary.Add("No clear reason OR flimsy reason(s) for camera operator to be recording ?", 10);
+            checkListDictionary.Add("Footage is shot in portrait mode ?", 10);
+            checkListDictionary.Add("Trail Camera Footage ?", 10);
+            checkListDictionary.Add("CCTV Footage ?", 15);
+            checkListDictionary.Add("Footage takes place at night, or with limited visibility ?", 15);
+            checkListDictionary.Add("Footage uses Infra Red or similar night camera technology ?", 15);
+            checkListDictionary.Add("Footage ends abruptly without explanation ?", 15);
+            checkListDictionary.Add("Footage is cropped, has deliberate edits or lacks context ?", 20);
+            checkListDictionary.Add("Footage comes with elaborate backstory ?", 25);
+            checkListDictionary.Add("Contains the phrase \"There was no one else present at the time\" ?", 30);
+            checkListDictionary.Add("Footage is from CCTV that has no clear reason to exist - e.g.CCTV in a residential home looking at nothing in particular ?", 30);
+            checkListDictionary.Add("Camera operator makes no attempt to approach subject where reasonably applicable ?", 30);
+            checkListDictionary.Add("Footage is unreasonably shakey and / or contains unnecessary zooms OR camera operator looks away from the subject?", 35);
+            checkListDictionary.Add("Footage resolution is lower than 720p ?", 35);
+            checkListDictionary.Add("Footage depicts known graphical errors / technical glitches ?", 40);
+            checkListDictionary.Add("Frames Per Second of Footage is less than 10fps ?", 40);
+            checkListDictionary.Add("Footage has large amount of digital compression ?", 45);
+            checkListDictionary.Add("Subject of footage is indistinct and / or some distance away ?", 45);
+            checkListDictionary.Add("Footage is CCTV footage recorded off a screen with a phone ?", 50);
+            checkListDictionary.Add("Footage is from a Redditor ?", 30);
+            checkListDictionary.Add("Footage is from a TikToker ?", 40);
+            checkListDictionary.Add("Footage is from a Youtuber ?", 50);
+            checkListDictionary.Add("Footage is from \"Paranormal Investigator\" Youtuber or similar ?", 60);
+            checkListDictionary.Add("Footage only contains \"mysterious sounds\", with no visual phenomenon ?", 70);
+        }
+
+        private void FufillCheckedListBoxObject() {
+            checkListBox1.Items.Clear();
+            checkListBox1.Items.AddRange(checkListDictionary.Keys.ToArray());
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            FufillCheckListValues();
+            FufillCheckedListBoxObject();
+        }
+
+        private void btn_clr_Click(object sender, EventArgs e)
+        {
+            checkListBox1.ClearSelected();
+            foreach (int i in checkListBox1.CheckedIndices) {
+                checkListBox1.SetItemCheckState(i, CheckState.Unchecked);
+            }
+            currentRating = 100;
+        }
+
+        private void checkListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_showRating_Click(object sender, EventArgs e)
+        {
+            currentRating = 100;
+            int ratingIndex = 0;
+
+            for (int i = 0; i < checkListBox1.Items.Count; i++) {
+                if (checkListBox1.GetItemChecked(i) == true) {
+                    currentRating -= checkListDictionary[checkListBox1.Items[i].ToString()];
+                }
+            }
+
+            if (currentRating > 79)
+            {
+                ratingIndex = 0;
+            }
+            else if (currentRating < 80 && currentRating > 59)
+            {
+                ratingIndex = 1;
+            }
+            else if (currentRating < 60 && currentRating > 39)
+            {
+                ratingIndex = 2;
+            }
+            else if (currentRating < 40 && currentRating > 19)
+            {
+                ratingIndex = 3;
+            }
+            else if (currentRating < 20 && currentRating > -1) {
+                ratingIndex = 4;
+            }
+            else if (currentRating < 0){
+                ratingIndex = 5;
+            }
+            longRating.Text = ratingOutputs[ratingIndex];
+            quickRating.Text = quickRatings[ratingIndex];
+            scoreValue.Text = currentRating.ToString();
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            checkListBox1.ClearSelected();
+            foreach (int i in checkListBox1.CheckedIndices)
+            {
+                checkListBox1.SetItemCheckState(i, CheckState.Unchecked);
+            }
+            currentRating = 100;
+            scoreValue.Text = currentRating.ToString();
+            longRating.Text = "";
+            quickRating.Text = "";
+        }
+
+        private void btn_about_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("The Paranormal Footage Credibility (PFC) Rating System is " +
+                "a scoring system designed to rate the potential credibility of any " +
+                "footage purporting to show paranormal activity (not the movie). This " +
+                "system starts by assuming 100% credibility for any given footage and " +
+                "converts this into a points system, 100 being the maximum or absolutely " +
+                "credible footage. As elements from the checklist are selected, they are " +
+                "individually weighted to represent the amount of credibility eroded by " +
+                "their inclusion. This is not to say it is impossible for footage with " +
+                "a low credibility score to be genuine, but instead represents if it is " +
+                "probable, rather than possible. Designed and built by DDD Wares, Nov 2021.");
+        }
+
+        private void btn_tips_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Paranormal Footage Credibility Rating System is first " +
+                "and foremost a tool - It should be used in conjuction with your best " +
+                "judgement and common sense. Beware of \"doubling\" up on check mark options - " +
+                "for instance, if camera footage uses night camera technology, you need not " +
+                "select both this option and the \"takes place during night\" option, as the " +
+                "former is weighted to include this factor. Generally speaking, if you are " +
+                "unsure as to what to choose between two options, pick the lower one on the " +
+                "list, as this will be weighted to include any prerequisites.");
+        }
+    } 
+}
